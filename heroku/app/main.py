0ef133deb3883bbe3680bb23261f4528f4077a67
kcwf3d <kcwf3d@outlook.com>
2021-07-20 18:14:17 -0500

    requirment update

---
+++ heroku/app/main.py
@@ -0,0 +1,142 @@
+
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc, cast, BigInteger
+from sqlalchemy.sql.roles import StatementOptionRole
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:myAkina93!@database-1.c6qqzgohsabf.us-east-2.rds.amazonaws.com:5432/project2"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.reflect()
+class Production(db.Model):
+    __tablename__ = 'production_data'
+class CropArea(db.Model):
+    __tablename__ = 'area_cleaned_data'
+class Yield(db.Model):
+    __tablename__= 'yield_cleaned_data'
+# db = SQLAlchemy(app)
+# # from models import db
+# # db.init_app(app)
+
+# class Production(db.Model):
+#     __tablename__ = 'production_data'
+
+#     Id = db.Column(db.Integer, primary_key=True)
+#     State= db.Column(db.String())
+#     Year = db.Column(db.String())
+#     Commodity= db.Column(db.String())
+#     Data_Item= db.Column(db.String())
+#     value= db.Column(db.String())
+ 
+        
+#     def __repr__(self):
+#         return '<Production %r>' % self.state
+
+@app.route('/prdpie/<state>')
+def pie(state):
+    results=db.session.query(
+    Production.Year,
+    Production.State,
+    Production.Commodity,
+    Production.Data_Item,
+    Production.Value,
+    Production.id
+    ).filter(Production.State==state).order_by(desc(Production.Value)).limit(5).all()
+
+    resultsJson= [{
+        'Year':result[0],
+        'State':result[1],
+        'Commodity': result[2],
+        'Data_Item': result[3],
+        'Value': result[4],
+        'id': result[5],
+    } for result in results]
+    return jsonify(resultsJson)
+
+@app.route('/areabar/<state>')
+def bar(state):
+    results=db.session.query(
+    CropArea.Year,
+    CropArea.State,
+    CropArea.Value,
+    CropArea.id
+    ).filter(CropArea.State==state).order_by((CropArea.Year)).all()
+
+    resultsJson= [{
+        'Year':result[0],
+        'State':result[1],
+        'Value': result[2],
+        'id': result[3],
+    } for result in results]
+    return jsonify(resultsJson)
+
+@app.route('/yieldline/<state>') 
+# @app.route('/yieldline/<state>/<commodityselected>')
+def line(state):
+    # if commodityselected == "all":
+        results=db.session.query(
+        Yield.Year,
+        Yield.State,
+        Yield.Commodity,
+        Yield.Data_Item,
+        Yield.Value,
+        Yield.id
+        ).filter(Yield.State==state).order_by((Yield.Year),(desc(Yield.Value))).all()
+
+
+        resultsJson= [{
+            'Year':result[0],
+            'State':result[1],
+            'Commodity': result[2],
+            'Data_Item': result[3],
+            'Value': result[4],
+            'id': result[5],
+        } for result in results]
+        return jsonify(resultsJson)
+    # else: 
+    #     # filter by commodity
+    #     results=db.session.query(
+    #     Yield.Year,
+    #     Yield.State,
+    #     Yield.Commodity,
+    #     Yield.Data_Item,
+    #     Yield.Value,
+    #     Yield.id
+    #     ).filter(Yield.State==state).filter(Yield.Commodity==commodityselected).order_by((Yield.Year),(desc(Yield.Value))).all()
+
+
+        resultsJson= [{
+            'Year':result[0],
+            'State':result[1],
+            'Commodity': result[2],
+            'Data_Item': result[3],
+            'Value': result[4],
+            'id': result[5],
+        } for result in results]
+        return jsonify(resultsJson)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+
+    # Run this when running on LOCAL server...
+    app.run(debug=True)
+
+    # ...OR run this when PRODUCTION server.
+    # app.run(debug=False)
---
+++ heroku/app/static/js/mapid.js
@@ -0,0 +1,318 @@
+function init() {
+  // console.log("init")
+  // Creating our initial map object
+  var myMap = L.map("mapid", {
+    center: [37.0902, -95.7129],
+    zoom: 4
+  });
+
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: "pk.eyJ1IjoiZXJpbi1uYXpldHRhIiwiYSI6ImNrcDRvaTExaTBib2cyeHQ4dWVyaDVlNWoifQ.DKcPTFaA051G8dTXau3ErQ",
+  }).addTo(myMap);
+
+
+  // Grabbing our GeoJSON data.
+  d3.json("/static/data/state_outlines.json").then(function (data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+      filter: function (feature) {
+        if (feature.properties.NAME == "Alaska" || feature.properties.NAME == "Hawaii" || feature.properties.NAME == "Puerto Rico") return false
+        else return true;
+      },
+      // style : function(feature) {
+      //   let cbsa = feature.properties.cbsafp;
+      //   poly_style = {
+      //     color: "black",
+      //     weight: 1,
+      //     fillColor: color_array[data_dict['index'][cbsa]],
+      //     fillOpacity: .9
+      //   }
+      //   return poly_style
+      // },
+      onEachFeature: function (feature, layer) {
+        let state_click = feature.properties.NAME;
+        layer.on({ click: OnClick });
+        layer.bindPopup("Check out the charts below to see data for crops in " + feature.properties.NAME);
+
+      }
+
+    }).addTo(myMap);
+  });
+};
+window.addEventListener('DOMContentLoaded', init);
+
+var saved_state_output = ""
+
+function OnClick(state_output) {
+  saved_state_output = state_output
+  // console.log(state_output)
+  // console.log(state_output.target.feature.properties.NAME.toUpperCase());
+  pieChart(state_output.target.feature.properties.NAME.toUpperCase());
+  barChart(state_output.target.feature.properties.NAME.toUpperCase());
+  lineGraph(state_output.target.feature.properties.NAME.toUpperCase())
+}
+
+function optChange(commodityselected) {
+  // console.log(saved_state_output.target.feature.properties.NAME.toUpperCase());
+  lineGraph(saved_state_output.target.feature.properties.NAME.toUpperCase(), commodityselected)
+}
+
+
+function pieChart(state, commodities) {
+  if (state === 'US TOTAL') {
+
+
+    d3.json(`/prdpie/${state}`).then(function (statedata) {
+      var comLabel = statedata.map(sdata => sdata.Commodity)
+      var mydata = statedata.map(sdata => sdata.Value)
+      // console.log(mylabels)
+      // console.log(mydata)
+      const data = {
+
+        labels: comLabel,
+        datasets: [{
+          label: `${state}`,
+          data: mydata,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(100, 205, 86)',
+            'rgb(255, 205, 255)',
+          ],
+          hoverOffset: 4
+        }]
+      };
+
+      const config = {
+        type: 'pie',
+        data: data,
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: `${state}'s Top 5 Crop Production by Value`
+            }
+          }
+        }
+      };
+
+      var myChart = new Chart(
+        document.getElementById('pieChart'),
+        config
+      );
+      lineGraph(state, comLabel)
+    });
+  } else {
+    d3.json(`/prdpie/${state}`).then(function (statedata) {
+      var comLabel = statedata.map(sdata => sdata.Commodity)
+      var mydata = statedata.map(sdata => sdata.Value)
+      // console.log(mylabels)
+      // console.log(mydata)
+      var myChart = Chart.getChart(`pieChart`)
+      // removeData(myChart)
+      addData(myChart, comLabel, mydata, state)
+      lineGraph(state, comLabel)
+    })
+  }
+}
+
+function addData(chart, label, data, state) {
+  // console.log(chart.data.datasets)
+  chart.data.labels = label;
+  chart.options.plugins.title.text = `${state}'s Top 5 Crop Production by Value`;
+  chart.data.datasets[0].data = data;
+  // console.log(chart.data.datasets.data)
+  chart.update();
+}
+function addDataBar(chart, label, data, state) {
+  // console.log(chart.data.datasets)
+  chart.data.labels = label;
+  chart.options.plugins.title.text = `${state}'s Yearly Crop Area`;
+  chart.data.datasets[0].data = data;
+  // console.log(chart.data.datasets.data)
+  chart.update();
+}
+function addDataLine(chart, datasets, state) {
+  // console.log(chart.data.datasets)
+  // chart.data.labels = label;
+  chart.options.plugins.title.text = `${state}'s Historical Crop Yields`;
+  chart.data.datasets = datasets;
+  // console.log(chart.data.datasets.data)
+  chart.update();
+}
+function removeData(chart) {
+  // console.log(chart.data.labels)
+  chart.data.labels = [];
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data = [];
+  });
+  chart.update();
+}
+pieChart('US TOTAL');
+barChart('US TOTAL');
+// lineGraph('US TOTAL');
+
+function barChart(state) {
+  if (state === 'US TOTAL') {
+
+
+    d3.json(`/areabar/${state}`).then(function (statedata) {
+
+      var mylabels = statedata.map(sdata => sdata.Year);
+      var mydata = statedata.map(sdata => parseInt(sdata.Value.replaceAll(',', '')));
+      // console.log(mylabels);
+      // console.log(mydata);
+      const data = {
+        labels: mylabels,
+        datasets: [{
+          label: `${state}`,
+          data: mydata,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 99, 132)',
+          ],
+          hoverOffset: 4
+        }]
+      };
+
+      const config = {
+        type: 'bar',
+        data: data,
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: `${state}'s Yearly Crop Area`
+            }
+          }
+        }
+      };
+
+      var myChart = new Chart(
+        document.getElementById('barChart'),
+        config
+      );
+    });
+  } else {
+    d3.json(`/areabar/${state}`).then(function (statedata) {
+      var mylabels = statedata.map(sdata => sdata.Year)
+      var mydata = statedata.map(sdata => parseInt(sdata.Value.replaceAll(',', '')))
+      // console.log(mylabels)
+      // console.log(mydata)
+      var myChart = Chart.getChart(`barChart`)
+      // removeData(myChart)
+      addDataBar(myChart, mylabels, mydata, state)
+    })
+  }
+}
+function lineGraph(state, commodities) {
+  if (state === 'US TOTAL') {
+    d3.json(`/yieldline/${state}`).then(function (statedata) {
+      console.log(statedata)
+      var datasets = []
+      commodities.forEach(commodity => {
+        var commodityData = statedata.filter(row => row.Commodity === commodity)
+        var values = commodityData.map(row => row.Value);
+        var dataset = {
+          label: commodity,
+          data: values,
+          fill: false,
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(100, 205, 86)',
+            'rgb(255, 205, 255)',
+          ],
+          hoverOffset: 4
+        }
+        datasets.push(dataset)
+      })
+      var mylabels = statedata.map(sdata => sdata.Year)
+      // var mydata = statedata.map(sdata => (sdata.Value.replaceAll(',', '')))
+      console.log(mylabels)
+      // console.log(mydata)
+      const data = {
+        labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+
+        datasets: datasets
+      };
+
+      const config = {
+        type: 'line',
+        data: data,
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: `${state}'s Top 5 Crops Yearly Yield`
+            }
+          }
+        }
+      };
+
+      var myChart = new Chart(
+        document.getElementById('lineGraph'),
+        config
+      );
+    });
+  } else {
+    d3.json(`/yieldline/${state}`).then(function (statedata) {
+      var mylabels = statedata.map(sdata => sdata.Year)
+      var datasets = []
+      commodities.forEach(commodity => {
+        var commodityData = statedata.filter(row => row.Commodity === commodity)
+        var values = commodityData.map(row => row.Value)
+        var dataset = {
+          label: commodity,
+          data: values,
+          fill: false,
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(100, 205, 86)',
+            'rgb(255, 205, 255)',
+          ],
+          hoverOffset: 4
+        }
+        datasets.push(dataset)
+      })
+      // console.log(mylabels)
+      // console.log(mydata)
+      var myChart = Chart.getChart(`lineGraph`)
+      // removeData(myChart)
+      addDataLine(myChart, datasets, state)
+    })
+  }
+}
+
+// labels: mylabels,
+        // datasets: [{
+        //   label: `${state}`,
+        //   data: mydata,
+        //   backgroundColor: [
+        //     'rgb(255, 99, 132)',
+        //     'rgb(54, 162, 235)',
+        //     'rgb(255, 205, 86)',
+        //     'rgb(100, 205, 86)',
+        //     'rgb(255, 205, 255)',
+        //   ],
+        //   hoverOffset: 4
+        // }]
